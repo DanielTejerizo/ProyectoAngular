e42bd54725fcb2f12d7e125fd646e694c0451bb6
DanielTejerizo <daniel.tejmor@educa.jcyl.es.com>
2024-05-19 16:44:05 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { MenuComponent } from './menu/menu.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenuComponent],
+  imports: [RouterOutlet, MenuComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
