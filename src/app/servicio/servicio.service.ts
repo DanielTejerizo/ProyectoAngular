f9a701d127890642164c81de6467fa5d3f102538
DanielTejerizo <daniel.tejmor@educa.jcyl.es.com>
2024-05-19 17:42:29 +0200

    .

--- src/app/servicio/servicio.service.ts
+++ src/app/servicio/servicio.service.ts
@@ -1,10 +1,16 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicioService {
+  private apiUrl = 'http://localhost/api.php';
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
+
+  obtenerProductos(): Observable<any> {
+    return this.http.get(`${this.apiUrl}?ruta=productos`);
+  }
 }
