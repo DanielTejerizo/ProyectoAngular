98f607499c4047111748c5c98f61997e71fdb56a
DanielTejerizo <daniel.tejmor@educa.jcyl.es.com>
2024-05-19 17:04:39 +0200

    .

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MaterialModulo } from '../../_modulo/MaterialModulo';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, MaterialModulo],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
