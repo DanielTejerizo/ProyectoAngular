52a78a385775720f74a124c877b57ea37c3f441e
DanielTejerizo <daniel.tejmor@educa.jcyl.es.com>
2024-05-19 16:30:53 +0200

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LibroInsertarComponent } from './libro-insertar/libro-insertar.component';
+import { LibroConsultarComponent } from './libro-consultar/libro-consultar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'insertar', component:LibroInsertarComponent},
+    {path: 'consultar', component:LibroConsultarComponent}
+];
