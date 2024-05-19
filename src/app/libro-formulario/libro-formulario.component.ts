1195bbb7d8d729bb60664bbdf2283f2092666f08
DanielTejerizo <daniel.tejmor@educa.jcyl.es.com>
2024-05-19 16:19:52 +0200

    .

---
+++ src/app/libro-formulario/libro-formulario.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-libro-formulario',
+  standalone: true,
+  imports: [],
+  templateUrl: './libro-formulario.component.html',
+  styleUrl: './libro-formulario.component.css'
+})
+export class LibroFormularioComponent {
+
+}
