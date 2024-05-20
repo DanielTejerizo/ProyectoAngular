08399e065146aade040d6465e873ffecf79bed04
DanielTejerizo <daniel.tejmor@educa.jcyl.es.com>
2024-05-20 19:02:40 +0200

    .

--- src/app/libro-insertar/libro-insertar.component.ts
+++ src/app/libro-insertar/libro-insertar.component.ts
@@ -8,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './libro-insertar.component.css'
 })
 export class LibroInsertarComponent {
+  libro = {
+    titulo: '',
+    autor: '',
+    ano: null,
+    genero: '',
+    imagen: null as File | null
+  };
 
+  onSubmit() {
+    console.log(this.libro);
+  }
+
+  onImageChange(event: Event) {
+    const element = event.target as HTMLInputElement;
+    if (element.files && element.files.length > 0) {
+      const file = element.files[0];
+      this.libro.imagen = file;
+    }
+  }
+  
 }
